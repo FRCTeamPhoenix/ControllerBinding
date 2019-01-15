79b13a8d07c790e3bf288cc40b893b209887dada
Joshua <joshburdick1@hotmail.com>
2019-01-14 20:59:55 -0500

    Added user input and started input parser

--- src/main/java/frc/robot/InputManager.java
+++ src/main/java/frc/robot/InputManager.java
@@ -5,15 +5,44 @@ import com.grack.nanojson.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class InputManager{
     private Map<String, String> m_axes;
 
     public InputManager(){
+        
+        Scanner controlChoice = new Scanner(System.in);
+        System.out.println("Enter the Function: ");
+        String controlFunction = controlChoice.nextString();
+        String controlAssign = new Scanner(System.in);
+        System.out.println("Enter the Binding: ");
+        String controlBind = controlAssign.nextString();
+        reader.close();
 
+        m_axes.put(controlFunction, controlBind);
     }
 
     public double getValue(String axisName){
+
+        this.stick = new Joystick(0);
+
+        String test = m_axes.indexOf(controlBind);
+        String[] tokens = test.split(" ");
+
+        for(String t : tokens) {
+            if(t = "Axis") {
+
+            }
+            else if(t = "Button") {
+
+            }
+            else {
+                System.out.println("Invalid Control Binding");
+            }
+        }
+
         return 1.0;
     }
 
